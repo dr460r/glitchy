540f7919d4434fb860d8dc2ebc3050b134e3a138
Dragor Draganović <dragor.draganovic@gmail.com>
2025-04-06 20:03:15 +0200

    feat: implement proper error handling

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
-use std::error::{Error};
 use std::result::{Result};
 use std::net::{TcpStream};
 use std::io::{BufRead, BufReader, Read, Write};
 use sha2::{Sha256, Digest};
 
+#[derive(Debug)]
+enum DlErr {
+    ConErr,
+    ReqErr,
+    ResErr,
+}
 
-fn dl_chunk(addr: &str, start: usize, end: usize) -> Result<(Vec<u8>, usize), Box<dyn Error>>{
-    let mut stream = TcpStream::connect(addr)?;
+type Chunk = (Vec<u8>, usize);
+
+fn dl_chunk(addr: &str, start: usize, end: usize) -> Result<Chunk, DlErr>{
+    let mut stream = TcpStream::connect(addr).map_err(|_| DlErr::ConErr)?;
 
     let req = format!(
         "GET / HTTP/1.1\r\n\
@@ -18,47 +25,71 @@ fn dl_chunk(addr: &str, start: usize, end: usize) -> Result<(Vec<u8>, usize), Bo
 
     println!("[REQ] Range: {} - {}", start, end);
 
-    stream.write_all(req.as_bytes())?;
-    stream.flush()?;
+    stream.write_all(req.as_bytes()).map_err(|_| DlErr::ReqErr)?;
+    stream.flush().map_err(|_| DlErr::ReqErr)?;
 
     let mut buf_r = BufReader::new(stream);
+    let mut exp_size: Option<usize> = None;
 
     loop {
         let mut line = String::new();
         let _ = buf_r.read_line(&mut line);
-        if line == "\r\n" {
-            break;
-        }
-        if line.to_lowercase().starts_with("content-length:") {
-            if let Some(cl_val) = line.split(':').nth(1) {
-                let cnt_len_head = cl_val.trim().parse::<usize>()?;
-                println!("[RES] Length (expect): {}", cnt_len_head);
-            }
+
+        if line == "\r\n" { break; }
+
+        if let Some(val) = line
+            .strip_prefix("Content-Length:")
+            .and_then(|x| x.trim().parse::<usize>().ok()) 
+        {
+            exp_size = Some(val);
+            println!("[RES] Length (expect): {}", val);
         }
     }
 
     let mut data: Vec<u8> = Vec::new();
-    let size = buf_r.read_to_end(&mut data)?;
+    let size = buf_r.read_to_end(&mut data).map_err(|_| DlErr::ResErr)?;
 
     println!("[RES] Lenght (actual): {}\n", size);
+
+    if let Some(exp) = exp_size {
+        if size == 0 && exp > 0 {
+            return Err(DlErr::ResErr);
+        }
+    }
 
     Ok((data, size))
 }
 
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<(), DlErr> {
     let srv_url: &str = "127.0.0.1:8080";
-    const CHUNK_SIZE: usize = 100000;
+    const CHUNK_SIZE: usize = 100 * 1024;
 
     let mut start = 0;
     let mut end = CHUNK_SIZE;
     let mut data: Vec<u8> = Vec::new();
+
     loop {
-        let (chunk, size) = dl_chunk(srv_url, start, end)?;
-        if size == 0 { break; }
-        data.extend(chunk);
-        start += size;
-        end = start + CHUNK_SIZE;
+        match dl_chunk(srv_url, start, end) {
+            Ok((cdata, csize)) => {
+                if csize == 0 { break };
+                data.extend(cdata);
+                start += csize;
+                end = start + CHUNK_SIZE;
+            },
+            Err(DlErr::ConErr) => {
+                println!("[ERR] Connectin error");
+                return Err(DlErr::ConErr);
+            },
+            Err(DlErr::ReqErr) => {
+                println!("[ERR] Request error. Trying again...");
+                continue;
+            },
+            Err(DlErr::ResErr) => {
+                println!("[ERR] Response error. Trying again...");
+                continue;
+            }
+        }
     }
 
     let mut sha = Sha256::new();
@@ -66,7 +97,6 @@ fn main() -> Result<(), Box<dyn Error>> {
     let hash = format!("{:x}", sha.finalize());
 
     println!("Length of the data: {}", data.len());
-
     println!("SHA-256 hash of the data: {}", hash);
 
     Ok(())
