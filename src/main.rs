5494a3aad558e4667f479f8d74f9290ec2b306f4
Dragor Draganović <dragor.draganovic@gmail.com>
2025-04-06 14:27:42 +0200

    feat: add sha2 dep and draft the program flow

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::{Error};
+use std::result::{Result};
+//use std::net::{TcpStream};
+//use sha2::{Sha256, Digest};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // Server connection setup
+
+    loop {
+        // TODO: Download Chunk
+        // Adjust Range
+        // If end, break
+    }
+
+    // Result
+    Ok(())
 }
